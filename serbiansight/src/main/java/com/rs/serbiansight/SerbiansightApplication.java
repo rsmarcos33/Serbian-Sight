9b2c92fd0ccc1e5ad8a441d382aed3e599d5c60c
rsmarcos33 <itmarko33@gmail.com>
2022-05-24 17:20:18 +0200

    Initial commit.

---
+++ serbiansight/src/main/java/com/rs/serbiansight/SerbiansightApplication.java
@@ -0,0 +1,13 @@
+package com.rs.serbiansight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerbiansightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerbiansightApplication.class, args);
+	}
+
+}
