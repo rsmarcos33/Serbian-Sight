faaa6d4b1860c56eac58a0582e95eaa31fdb2c90
rsmarcos33 <itmarko33@gmail.com>
2022-06-07 15:20:03 +0200

    Update UserServiceImplementation.java
    Deleting unused imports

--- serbiansight/src/main/java/com/service/implementation/UserServiceImplementation.java
+++ serbiansight/src/main/java/com/service/implementation/UserServiceImplementation.java
@@ -6,8 +6,6 @@ import com.repository.UserRepository;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.Optional;
